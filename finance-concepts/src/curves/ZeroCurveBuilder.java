ed58dcab84387a4b466124d4be6ed47c707e886c
Christopher Aguirre <chris.aguirre11@gmail.com>
2015-12-22 17:42:07 -0500

    Got ZeroCurveBuilder working successfully

--- finance-concepts/src/curves/ZeroCurveBuilder.java
+++ finance-concepts/src/curves/ZeroCurveBuilder.java
@@ -1,41 +1,27 @@
 package curves;
 
-import java.util.List;
-
 public class ZeroCurveBuilder {
-	
-	double[] yieldCurveRates;
-	double rZ1 = 0.0; //Zero Rate for Year 1
-	double investmentAmount = 1.0;
-	double[] zeroCurveRates;
-	
-	public ZeroCurveBuilder(double[] yieldCurveRates, double investmentAmount) {
-		this.yieldCurveRates = yieldCurveRates;
-		this.investmentAmount = investmentAmount;
-	}
 	
 	public static void main(String[] args) {
 		double[] yieldCurveRates = {
-				0.05, 0.06, 0.07
+				0.05, 0.06, 0.07, 0.08, 0.09
 		};
 		
-		ZeroCurveBuilder zBuilder = new ZeroCurveBuilder(yieldCurveRates, 1.0);
-		zBuilder.calculateZeroRates();
+		double[] zeroCurveRates = ZeroCurveBuilder.calculateZeroCurveRates(yieldCurveRates, 1.0);
+		System.out.println();
 	}
 	
 	/**
-	 * Generates a zero curve from the yieldCurve Rates
-	 */	
-	public double[] calculateZeroRates() {
+	 * Generates a zero curve from the yieldCurveRates and investmentAmount
+	 */
+	public static double[] calculateZeroCurveRates(double[] yieldCurveRates, double investmentAmount) {
 		
-		zeroCurveRates = new double[yieldCurveRates.length]; 
+		double[] zeroCurveRates = new double[yieldCurveRates.length]; 
 		
-		//Calcuate rZ1
+		//Set 1st Zero Curve Rate to be the same as 1st Yield Curve Rate
 		zeroCurveRates[0] = yieldCurveRates[0];
-		rZ1 = zeroCurveRates[0];
 		
-		
-		int max = 3;
+		int max = yieldCurveRates.length;
 		for(int i = 2; i <= max; i++) {
 			
 			double yieldRateForCurrentYearPoint = yieldCurveRates[i-1];
@@ -44,25 +30,29 @@ public class ZeroCurveBuilder {
 			
 			for(int y = 1; y <= i; y++) {
 				if(y == i) {
-					double currentPresentValue = (1 + yieldRateForCurrentYearPoint) 
+					
+					double sumOfPreviousPresentValues = 0.0;
+					for(int z = 0; z < currentPresentValues.length; z++) {
+						sumOfPreviousPresentValues += currentPresentValues[z];
+					}
+					
+					double leftSideOfEquation = 1 - sumOfPreviousPresentValues;
+					double leftSideOfEquation1 = Math.pow(leftSideOfEquation, (1.0/y));
+					double lastCashFlowAmount = 1 + yieldRateForCurrentYearPoint;
+					double rightSideOfEquation1 = Math.pow(lastCashFlowAmount, (1.0/y));
+					double rightSideOfEquation2 = rightSideOfEquation1 - leftSideOfEquation1;
+					double zeroRateForCurrentYearPoint = rightSideOfEquation2 / leftSideOfEquation1;
+					
+					zeroCurveRates[i - 1] = zeroRateForCurrentYearPoint;
+				}
+				else {
+					double currentPresentValue = yieldRateForCurrentYearPoint 
 							/ Math.pow((investmentAmount + zeroCurveRates[y-1]), y);
 					
 					currentPresentValues[y-1] = currentPresentValue;
 				}
-				double currentPresentValue = yieldRateForCurrentYearPoint 
-						/ Math.pow((investmentAmount + zeroCurveRates[y-1]), y);
-				
-				currentPresentValues[y-1] = currentPresentValue;
-				
-				
 			}
-			
 		}
-		
-		
-		
 		return zeroCurveRates;
-		
 	}
-
 }
